c1cec5e7455b692da9cbe6ec83a2252a4470da6e
Jon Bennett <jdbennet@ca.ibm.com>
2017-10-19 13:21:40 -0400

    Initial archive tests.

---
+++ tests/server-tests/zip-files.js
@@ -0,0 +1,25 @@
+const assert = require('chai').assert;
+
+const admZip = require('adm-zip');
+const path    = require('path');
+const _       = require('lodash');
+const S       = require('String');
+
+// Read the archive file into a typedArray
+
+const path_to_file = path.join(__dirname, '../archives/running.cbz')
+
+describe('Can extract CBZ file', function() {
+  debugger;
+    it('Should return the first page of the archive', function() {
+
+      var zip = new admZip(path_to_file);
+      var zip_entries = zip.getEntries();
+      zip_entries = _.filter(zip_entries, zip_entry => {
+        return !S(zip_entry.entryName).startsWith('__')
+      })
+
+      debugger;
+
+    });
+});
