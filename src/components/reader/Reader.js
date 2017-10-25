5d84aa182d35162d5071a8ccd0841319563a78af
Jon Bennett <jdbennet@ca.ibm.com>
2017-10-25 15:02:35 -0400

    Stack images in Reader pane.

--- src/components/reader/Reader.js
+++ src/components/reader/Reader.js
@@ -3,8 +3,14 @@ import { browserHistory, Router } from 'react-router'
 import { Provider } from 'react-redux'
 import PropTypes from 'prop-types'
 
+import './Reader.scss'
+
 class Reader extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {pages: ['bone.jpg', 'bone_02.jpg', 'bone_03.jpg']}
+  }
 
   shouldComponentUpdate () {
     return false
@@ -14,11 +20,21 @@ class Reader extends React.Component {
     //this.fetchData();
   }
 
-
+  handleClick(e){
+    debugger;
+    alert('click!');
+  }
 
   render () {
+
+    let pages = this.state.pages.map( (page, index) =>{
+        return <div className='page' onClick={() =>this.handleClick({index})} key={page}><img src={'/icons/' +page} /></div>
+    })
+
     return (
-      <div>Your Text Here..</div>
+      <div className='viewport'>
+        {pages}
+      </div>
     )
   }
 }
