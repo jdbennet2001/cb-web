09033b397e4a99032a052d02cc54b067d0d5368a
jdbennetibm <jdbennet2001@yahoo.com>
2017-10-28 18:46:18 -0400

    Swiping

--- src/components/reader/Reader.js
+++ src/components/reader/Reader.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { browserHistory, Router } from 'react-router'
 import { Provider } from 'react-redux'
 import PropTypes from 'prop-types'
+import ReactSwipe from 'react-swipe';
 
 import './Reader.scss'
 
@@ -32,49 +33,32 @@ updateWindowDimensions() {
   this.setState(state);
 }
 
+handleClick(){
+   this.reactSwipe.next();
+}
 
 
-  handleClick(index){
-    let pages =  this.state.pages;
-    let state = this.state;
-    let from = state.offset;
-    let to =  Math.max(state.offset - state.width, -1 *( state.pages.length -1) * state.width);
-    this.slide(from, to);
-  }
-
-  slide(from, to){
-
-    let delta = (to - from) / 100;
-    let counter = 0;
-
-    let timer = setInterval( () => {
-
-      let state = this.state;
-      state.offset += delta;
-      this.setState(state);
-
-      if ( ++ counter  == 100){
-        clearInterval(timer);
-      }
-
-    }, 10)
-
-  }
 
   render () {
 
-    let pages = this.state.pages.map( (page, index) =>{
-        const style = { width: this.state.width };
-        return <div className='page' style={style} key={page.image}><img src={'/icons/' +page} /></div>
+    // let pages = this.state.pages.map( (page, index) =>{
+    //     const style = { width: this.state.width };
+    //     return <div className='page' style={style} key={'bone' + index}><img src={'/icons/' +page} /></div>
+    // })
+
+    // let style = {height : this.state.height, width: this.state.width};
+
+    let style={};
+
+    let pages = this.state.pages.map( (page,index) => {
+      return <img src={'/icons/' +page} style={style} key={index}/>
     })
 
-    let filmstrip_style = {left: this.state.offset, position:"absolute"}
-
     return (
-      <div className='viewport' onClick={() => {this.handleClick()}}>
-        <div className='filmstrip' style={filmstrip_style}>
+      <div className='reader' onClick={() => this.handleClick()} >
+       <ReactSwipe ref={reactSwipe => this.reactSwipe = reactSwipe}  className="carousel" swipeOptions={{continuous: false}}>
           {pages}
-        </div>
+      </ReactSwipe>
       </div>
     )
   }
