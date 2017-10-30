d19a24785be5c982f7b2a9db8dbe40f278d94633
Jon Bennett <jdbennet@ca.ibm.com>
2017-10-30 09:54:32 -0400

    Add title bar.

--- src/components/browser/HomeView.js
+++ src/components/browser/HomeView.js
@@ -4,12 +4,18 @@ import NavList from './NavList'
 import CoversView from './CoversView'
 
 export const HomeView = () => (
-  <div className='home-page'>
-    <div className='nav-pane'>
-      <NavList></NavList>
+  <div className='browser-page'>
+    <div className='title-bar'>
     </div>
-    <div className='content-pane'>
-      <CoversView router={this.props}></CoversView>
+    <div className='home-page'>
+      <div className='nav-pane'>
+        <NavList>
+        </NavList>
+      </div>
+      <div className='content-pane'>
+        <CoversView router={this.props}>
+        </CoversView>
+      </div>
     </div>
   </div>
 )
