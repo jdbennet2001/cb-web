4e012ce1aecf1d8a10671c6ca18b990a570cb6da
Jon Bennett <jdbennet@ca.ibm.com>
2017-12-21 15:20:12 -0500

    Update page animation logic for larger archives.

--- src/components/reader/Reader.js
+++ src/components/reader/Reader.js
@@ -2,16 +2,25 @@ import React from 'react'
 import { browserHistory, Router } from 'react-router'
 import { Provider } from 'react-redux'
 import PropTypes from 'prop-types'
-import ReactSwipe from 'react-swipe';
 import {connect}  from 'react-redux'
+import Swipeable from "react-swipeable";
 import _ from 'lodash';
 
 import './Reader.scss'
 
+const PAGE_0 =  "https://static.comicvine.com/uploads/original/0/40/3198571-6+flsann_2_33.jpg";
+const PAGE_1 =  "http://www.freaksugar.com/wp-content/uploads/2017/07/FLS_27_1.jpg";
+const PAGE_2 =   "http://image.11comic.com/images/proimg/160426051000449i/160426051000449i_6.jpg";
+
+
 class Reader extends React.Component {
+
+
 
 constructor(props) {
   super(props);
+
+  debugger;
 
     var hash = window.location.hash.substr(1);
 
@@ -22,138 +31,137 @@ constructor(props) {
     }, {});
 
     const archive = result.archive;
-    const length = parseInt(result.length);
+    const count = parseInt(result.length);
 
-  this.state = {
-    pages: [],
-    pos: 0,
-    home_icon: false,
-    archive,
-    length
-  };
+    this.state = {
+      paddingLeft: 0,
+      paddingRight: 0,
+      count,
+      archive,
+      page_number: 0,
+      page: `/page?archive=${encodeURIComponent(archive)}&number=${0}`
+    };
 
-  this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
 }
 
-componentDidMount() {
-  this.updateWindowDimensions();
-  window.addEventListener('resize', this.updateWindowDimensions);
+componentDidUpdate() {
+  // debugger;
+  // this.updateWindowDimensions();
+  // window.addEventListener('resize', this.updateWindowDimensions);
 
-  window.addEventListener("orientationchange", this.handleOrientationChange);
+  // window.addEventListener("orientationchange", this.handleOrientationChange);
 }
 
-componentWillUnmount() {
-  window.removeEventListener('resize', this.updateWindowDimensions);
-  window.removeEventListener("orientationchange", this.handleOrientationChange);
+// componentWillUnmount() {
+//   window.removeEventListener('resize', this.updateWindowDimensions);
+//   window.removeEventListener("orientationchange", this.handleOrientationChange);
+// }
+
+swiping(e, deltaX, deltaY, absX, absY, velocity) {
+  console.log("You're Swiping...", e, deltaX, deltaY, absX, absY, velocity);
 }
 
-
-getPages(position = 0, archive, length){
-
-     archive  = archive || this.state.archive;
-     length = length || this.state.length;
-
-    let pages = _.times(length, index =>{
-      return `/blank-svg-page.svg`;
-    })
-
-    let start_image = Math.max( position-1, 0 );
-    let end_image = Math.min(position + 2, length-1 );
-
-    for ( var i = start_image; i <= end_image; i++ ){
-      pages[i] = `/page?archive=${encodeURIComponent(archive)}&number=${i}`;
-    }
-
-    return pages;
+swipingLeft(e, absX) {
+  let state = Object.assign({}, this.state);
+  state.paddingRight = absX;
+  this.setState(state);
+  console.log("You're Swiping to the Left...", e, absX);
 }
 
-updateWindowDimensions() {
-  let pos = this.reactSwipe.getPos();
-  let state = this.state;
-  let width = this.isIpad() ? window.outerWidth +16 : window.outerWidth;
-  let pages = this.getPages(pos);
-  let height = window.innerHeight;
-    state = Object.assign(state, {
-      width,
-      height,
-      pages,
-      page: pos
-    });
-    this.setState(state);
+swipingRight(e, absX) {
+  let state = Object.assign({}, this.state);
+  state.paddingLeft = absX;
+  this.setState(state);
+  console.log("You're Swiping to the Right...", e, absX);
 }
 
-isIpad(){
-  return navigator.userAgent.indexOf('Chrome') == -1;
+swiped(e, deltaX, deltaY, isFlick, velocity) {
+
+  if (deltaX > 100) {
+    this.updatePage('FORWARD')
+  } else if (deltaX < -100) {
+    this.updatePage('BACK');
+  }
+
+
+  console.log("You Swiped...", e, deltaX, deltaY, isFlick, velocity);
 }
 
-handleOrientationChange(e){
-  //Force a page reload to get the correct aspect ratio
-  browserHistory.push(`${window.location.pathname}${window.location.hash}`);
+swipedUp(e, deltaY, isFlick) {
+  console.log("You Swiped Up...", e, deltaY, isFlick);
 }
 
-handleKeyPress(e){
-
+handleKeyPress(e) {
   const RIGHT_ARROW = 39;
   const LEFT_ARROW = 37;
 
-
-  if (e.keyCode == RIGHT_ARROW){
-   this.reactSwipe.next();
-  }else if (e.keyCode=== LEFT_ARROW){
-    this.reactSwipe.prev();
+  if (e.keyCode == RIGHT_ARROW) {
+    this.updatePage('FORWARD');
+  } else if (e.keyCode === LEFT_ARROW) {
+    this.updatePage('BACK');
   }
 }
 
-handleClick(e){
-  let state = this.state;
-  state.home_icon = !state.home_icon;
-  this.setState(state);
+
+rightClick(e){
+  e.preventDefault(); 
+  this.updatePage('FORWARD');
+  return false;
 }
+
+updatePage(direction) {
+
+   let state = Object.assign({}, this.state, {
+    paddingLeft: 0,
+    paddingRight: 0
+  });
+
+  let next_archive = this.nextIssue(state.archive);
+  let last_archive = this.previousIssue(state.archive);
+
+  let last_page = (state.page_number === state.count -1);
+  let first_page = (state.page_number === 0);
+
+  if ( last_page && !next_archive && direction === 'FORWARD' ){
+    return this.setState(state);
+  }
+
+  if ( first_page && !last_archive && direction === 'BACK' ){
+    return this.setState(state);
+  }
+
+  if ( last_page  &&  direction == 'FORWARD'){
+    state.archive = next_archive.location;
+    state.page_number = 0;
+  }else if ( first_page && direction === 'BACK' ){
+    state.archive = last_archive.location;
+    state.page_number = 0;
+  }else if (direction === 'FORWARD'){
+    state.page_number = Math.min(state.page_number+1, state.count-1);
+  }else{
+    state.page_number = Math.max(state.page_number-1, 0);
+  }
+
+
+  state.page = `/page?archive=${encodeURIComponent(state.archive)}&number=${state.page_number}`;
+  
+  this.setState(state);
+
+}
+
+pageRedraw(e){
+  if ( !e ){
+    return;
+  }
+
+  e.element.focus();
+}
+
 
 goBack(){
 
  browserHistory.push(`/`);
 
-}
-
-transitionHandler(){
-  let pos = this.reactSwipe.getPos();
-  let state = this.state;
-  let archive = state.archive;
-
-  let pages = this.getPages(pos);
-      state = Object.assign(state, {pages, pos} );
-  this.setState(state);
-}
-
-screen(){
-  //Tablet landscape
-  if (window.orientation == 0 || window.orientation == 180 ){
-    return {w: window.screen.width, h: window.screen.height-75, orientation: 'portrait'};
-  //Tablet portrait
-  }else if ( window.orientation == 90 || window.orientation == -90 ){
-    return {w: window.screen.height, h: window.screen.width-75, orientation: 'landscape'};
-  }
-  //Laptop
-  else{
-    return {w: window.outerWidth+16, h: window.innerHeight, orientation: 'laptop'};
-  }
-}
-
-handleIssueEnd(archive, length, e){
-
-  e.preventDefault();
-
-  let state = this.state;
-  state = Object.assign( state, {
-    pages: this.getPages(0, archive, length),
-    pos: 0,
-    home_icon: false,
-    archive,
-    length
-  });
-
-  this.setState(state);
 }
 
 nextIssue(archive){
@@ -166,49 +174,36 @@ nextIssue(archive){
     return next;
 }
 
-  render () {
-
-    let screen = this.screen();
-
-    let style = {height : screen.h, width: screen.w};
-
-    let orientation = window.orientation || 'n/a';
-
-    let pages = this.state.pages.map( (page,index) => {
-      return <div style={style} className={'page ' + screen.orientation} key={'parent-'+index+'-'+orientation} >
-                <img className='page_image' src={page}  key={index}/>
-            </div>
+previousIssue(archive){
+  //Code to move to next issue in directory
+    let issue = _.findIndex(this.props.files, file=>{
+      return file.location === archive;
     })
 
-    let reader_back = <div></div>;
-    if (this.state.home_icon){
-      reader_back = <div onClick={() =>this.goBack()} className='reader-back'><img src='/icons/home.png'></img></div>
-    }
+    let next = _.nth(this.props.files, --issue);
+    return next;
+}
 
-    let swipeOptions = {
-      continuous: false,
-      transitionEnd: this.transitionHandler.bind(this),
-      startSlide: this.state.pos || 0
-    };
+  render () {
 
-    let swipe_key = `${this.state.archive}_${pages.length}_${orientation}`
+    let divStyle = this.state;
+    let page = this.state.page;
 
-    let next_file = this.nextIssue(this.state.archive);
-    let next_issue = <div></div>
-    if ( next_file && (this.state.pos === pages.length -1) ){
-        next_issue = <div className='nextIssueThumbnail' onClick={(e) => this.handleIssueEnd(next_file.location, next_file.length, e)}>
-                        <img src={'/cover/' +encodeURIComponent(next_file.name)}></img>
-                    </div>
-    }
-    return (
-      <div className='reader' tabIndex='0' onClick={()=>this.handleClick()} onKeyDown={(event) => this.handleKeyPress(event)} >
-       {reader_back}
-       {next_issue}
-       <ReactSwipe key={swipe_key} ref={reactSwipe => this.reactSwipe = reactSwipe}  className="carousel" swipeOptions={swipeOptions}>
-          {pages}
-      </ReactSwipe>
-      </div>
-    )
+    this.canvas = <Swipeable
+        className="reader"
+        onSwipingLeft={this.swipingLeft.bind(this)}
+        onSwipingRight={this.swipingRight.bind(this)}
+        onSwiped={this.swiped.bind(this)}
+        onKeyDown={event => this.handleKeyPress(event)} 
+        onContextMenu={event => this.rightClick(event)}
+        tabIndex="0"  
+        ref = {e => this.pageRedraw(e)}
+        key={this.state.page_number} >
+        <img src={page} style={divStyle} className="App-logo" alt="logo" />
+      </Swipeable>
+    
+    return this.canvas;
+
   }
 }
 
